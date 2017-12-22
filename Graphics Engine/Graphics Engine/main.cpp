b883c5e47954475e939007c920fa4ed5bd8fa7a1
Tony Tarng <tonytarng1098@gmail.com>
2017-12-23 00:45:47 +0800

    Add FBOs

--- Graphics Engine/Graphics Engine/main.cpp
+++ Graphics Engine/Graphics Engine/main.cpp
@@ -20,7 +20,9 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void renderScene();
+void renderScene(Shader wallShader, unsigned int wallVAO);
+unsigned int initializeReflectionFBO();
+unsigned int initializeRefractionFBO();
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -36,6 +38,7 @@ bool firstMouse = true;
 float deltaTime = 0.0f;    // time between current frame and last frame
 float lastFrame = 0.0f;
 
+unsigned int textureColorBuffer;
 int main()
 {
     
@@ -72,52 +75,103 @@ int main()
     }
     
     // ------- configure global opengl state -------
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
     
     // ------------------ shaders ------------------
     
-    Shader ourShader("./shader.vs", "./shader.frag");
+    Shader wallShader("./wallShader.vs", "./wallShader.frag");
     
     Shader waterShader("./water.vs", "./water.frag");
+    
+    Shader screenShader("./screenShader.vs", "./screenShader.frag");
     
     // ----------------- data processing ----------------
     
     // vertex data
     float water[] = { // all y set to 0
-        -2, -2, // botton left
-        2, -2, // bottom right
-        2, -6, // top right
-        -2, -2, // botton left
-        2, -6, // top right
-        -2, -6 // top left
+        -8, 8, // botton left
+        8, 8, // bottom right
+        8, -8, // top right
+        -8, 8, // botton left
+        8, -8, // top right
+        -8, -8 // top left
     };
 
+    float wall[] = { // RENDER WITH GL_QUADS
+        // position texture
+        -2, -2, -5,  0, 1,
+        -2, -2, 5,   0, 0,
+        2, -2, 5,    1, 0,
+        2, -2, -5,   1, 1,
+        -2, 2, -5,   0, 1,
+        -2, -2, -5,  0, 0,
+        2, -2, -5,   1, 0,
+        2, 2, -5,    1, 1,
+        2, 2, 5,     0, 1,
+        2, -2, 5,    0, 0,
+        -2, -2, 5,   1, 0,
+        -2, 2, 5,    1, 1,
+        -2, 2, 5,    0, 1,
+        -2, -2, 5,   0, 0,
+        -2, -2, -5,  1, 0,
+        -2, 2, -5,   1, 1,
+        2, 2, -5,    0, 1,
+        2, -2, -5,   0, 0,
+        2, -2, 5,    1, 0,
+        2, 2, 5,     1, 1
+    };
     
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    float quadVertices[] = { // for rendering to screen space
+        // positions   // texCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f, 1.0f
+    };
+    
+    unsigned int waterVBO, waterVAO, wallVBO, wallVAO;
+    glGenVertexArrays(1, &waterVAO);
+    glGenBuffers(1, &waterVBO);
+    glGenVertexArrays(1, &wallVAO);
+    glGenBuffers(1, &wallVBO);
     
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindVertexArray(waterVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, waterVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(water), water, GL_STATIC_DRAW);
-    
-
     // position attribute
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    /*
+    
+    glBindVertexArray(wallVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, wallVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(wall), wall, GL_STATIC_DRAW);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
     // texture coord attribute
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-     */
+    
+    // screen quad VAO
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    
     
     // -------------- textures ---------------------
     
-    /*
-     
-    unsigned int texture1, texture2;
+    unsigned int texture1;
     
     // texture 1
     glGenTextures(1, &texture1);
@@ -131,7 +185,7 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load("/Users/mac/Desktop/Graphics\ Engine/textures/container.jpg", &width, &height, &nrChannels, 0);
+    unsigned char *data = stbi_load("../textures/marble.bmp", &width, &height, &nrChannels, 0);
     if (data)
     {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
@@ -143,39 +197,23 @@ int main()
     }
     stbi_image_free(data);
     
-    // texture 2
-    glGenTextures(1, &texture2);
-    glBindTexture(GL_TEXTURE_2D, texture2);
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // load image, create texture and generate mipmaps
-    data = stbi_load("/Users/mac/Desktop/Graphics\ Engine/textures/awesomeface.png", &width, &height, &nrChannels, 0);
-    if (data)
-    {
-        // note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-    }
-    stbi_image_free(data);
+    // ------------ shader configuration ---------------
+    
+    wallShader.use();
+    wallShader.setInt("texture1", 0);
+    
+    screenShader.use();
+    screenShader.setInt("screenTexture", 0);
+
+    // ----------- frame buffer configuration ----------
+    
+    unsigned int reflectionFBO = initializeReflectionFBO();
+    unsigned int refractionFBO = initializeRefractionFBO();
     
     
-    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
-    ourShader.use(); // activate/use the shader before setting uniforms
-    ourShader.setInt("texture1", 0);
-    ourShader.setInt("texture2", 1);
-     
-     */
+    // --------------- drawing mode ---------------------
     
-    // drawing mode
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // uncomment to see that we only rendered a single quad in screen space using FBOs
     
     // --------------- render loop ------------------------
     while (!glfwWindowShouldClose(window))
@@ -188,19 +226,23 @@ int main()
         // input
         processInput(window);
         
+        // ------------------ 1st pass ---------------
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, reflectionFBO);
+        glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+        
         // rendering
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // bind textures on corresponding texture units
-        /*
+        // render wall
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-         */
+        renderScene(wallShader, wallVAO);
         
+        // render water
         waterShader.use();
+        glBindVertexArray(waterVAO);
         // do transformations
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -208,34 +250,115 @@ int main()
         // camera/view transformation
         glm::mat4 view = camera.GetViewMatrix();
         waterShader.setMat4("view", view);
-        
-        glBindVertexArray(VAO);
-        
         glm::mat4 model= glm::mat4(1.0f);
         waterShader.setMat4("model", model);
-        
         glDrawArrays(GL_TRIANGLES, 0, 6);
         
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
- 
+        // ------------------ 2nd pass ---------------
+        
+        // draw a quad plane with the attached framebuffer color texture
+        glBindFramebuffer(GL_FRAMEBUFFER, 0); // now bind back to default framebuffer
+        glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+        // clear all relevant buffers
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f); // set clear color to white (not really necessery actually, since we won't be able to see behind the quad anyways)
+        glClear(GL_COLOR_BUFFER_BIT);
+        screenShader.use();
+        glBindVertexArray(quadVAO);
+        glBindTexture(GL_TEXTURE_2D, textureColorBuffer);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        
         // check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
     
     // deallocate resources
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &waterVAO);
+    glDeleteBuffers(1, &waterVBO);
+    glDeleteFramebuffers(1, &reflectionFBO);
+    glDeleteFramebuffers(1, &refractionFBO);
+    // ToDo: Delete textures, rbo
     
     // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();
     return 0;
 }
 
-// draw everything aside from water
-void renderScene()
+// textureColorBuffer set as global var for convenience
+unsigned int initializeReflectionFBO()
 {
+    // reflection frame buffer
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    // create a color attachment texture
+    unsigned int depthBuffer;
+    glGenTextures(1, &textureColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorBuffer, 0);
+    // create a depth attachment texture
+    glGenTextures(1, &depthBuffer);
+    glBindTexture(GL_TEXTURE_2D, depthBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32, SCR_WIDTH, SCR_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthBuffer, 0);
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Reflection framebuffer is not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
     
+    return fbo;
+}
+
+unsigned int initializeRefractionFBO()
+{
+    // refraction frame buffer
+    unsigned int fbo;
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    // create a color attachment texture
+    unsigned int colorBuffer, depthRenderBuffer;
+    glGenTextures(1, &colorBuffer);
+    glBindTexture(GL_TEXTURE_2D, colorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBuffer, 0);
+    // create a depth render buffer attachment
+    glGenRenderbuffers(1, &depthRenderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_WIDTH, SCR_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer); // now actually attach it
+    // now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Refraction framebuffer is not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    
+    return fbo;
+}
+
+// draw everything aside from water
+void renderScene(Shader wallShader, unsigned int wallVAO)
+{
+    wallShader.use();
+    // do transformations
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    wallShader.setMat4("projection", projection);
+    // camera/view transformation
+    glm::mat4 view = camera.GetViewMatrix();
+    wallShader.setMat4("view", view);
+    
+    glBindVertexArray(wallVAO);
+    
+    glm::mat4 model= glm::mat4(1.0f);
+    wallShader.setMat4("model", model);
+    glDrawArrays(GL_QUADS, 0, 20);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
